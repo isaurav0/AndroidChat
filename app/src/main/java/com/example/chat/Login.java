6d1726eea071e50836523adda907a8c09f43e8e2
Saurav Pathak <saurab.pathak.0@gmail.com>
2019-12-17 13:54:32 +0545

    views

--- app/src/main/java/com/example/chat/Login.java
+++ app/src/main/java/com/example/chat/Login.java
@@ -3,16 +3,36 @@ package com.example.chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 public class Login extends AppCompatActivity {
 
-    Button login;
+    Button btn_login;
+    EditText input_username, input_password;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login= (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input_username = (EditText) findViewById(R.id.edit_email);
+                input_password = (EditText) findViewById(R.id.edit_password);` `
+                username = input_username.getText().toString();
+                password = input_password.getText().toString();
+                Toast.makeText(getApplicationContext(), username, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
